72aa0064985df26358692854e35c553b3dfd22e6
TimonAME <timon-a@outlook.at>
2023-11-27 12:48:39 +0100

    Semester 2 Core 2 gestartet

---
+++ 2.Semester/Core/IP-Address-Lookup/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- InputIP with search event -->
+    <InputIP @searchIP="search" />
+  </div>
+</template>
+
+<script setup>
+import InputIP from "./components/InputIP.vue";
+
+// event when search is clicked
+const search = (ip) => {
+  console.log("processing ip address: " + ip);
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
